e0c1c2c3eb31e309fa82b572720e65b9689fecf5
Jamie <jxberc@gmail.com>
2014-06-18 09:19:29 -0400

    Fix email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,18 @@ FlightBooker::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+  }
+
+
   config.action_mailer.default_url_options = { :host => 'http://flight-booker.herokuapp.com' }
 end
