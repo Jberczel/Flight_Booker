b5ca5adf637fa6fc37191283ea2ec85266f35845
Jamie <jxberc@gmail.com>
2014-06-18 08:45:46 -0400

    Edit config file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ FlightBooker::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'http://flight-booker.herokuapp.com }
+  config.action_mailer.default_url_options = { :host => 'http://flight-booker.herokuapp.com' }
 end
